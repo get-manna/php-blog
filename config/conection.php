706820bf56c7eb62d788a7a5c3052889ca174a1a
exitdump <jahidhasan100101@gmail.com>
2025-02-25 12:37:31 +0600

    add error reporting and connection error handling to database configuration

--- config/conection.php
+++ config/conection.php
@@ -1,6 +1,14 @@
 <?php 
-
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 
 $conn = mysqli_connect(hostname: "localhost",username: "root",password: "root",database: "blog");
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
 ?>
