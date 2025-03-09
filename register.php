39d510c6b57e86549ef92a6e3a86f0052d58aa7c
get-manna <mujiburrahmanmanna@gmail.com>
2025-03-09 04:24:28 -0600

    register form updated

--- register.php
+++ register.php
@@ -35,7 +35,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form Validator</title>
+    <title>Register Form </title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
